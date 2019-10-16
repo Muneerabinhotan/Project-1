3653896665f108f2d6daa5f127d30c751a4582b3
Muneera Bin Hotan <muneerabinhotan@gmail.com>
2019-10-16 20:12:40 +0300

    done

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tic Tac Toe Project
+
+## Table of Contents
+
+* [General](#general)
+* [How The Game Works](#how-the-game-works)
+* [Resources](#resources)
+
+## General
+
+General Assembly Software Engineering Immersive
+
+## How The Game Works
+
+* The game will start with two players X and O each one of them will try to score more.
+* The game keeps a track of the scores.
+* The players can reset the score.
+* The players can reset the game.
+
+
+## Resources
+* [View Demo] ()
+* Font [fontsquirrel](https://www.fontsquirrel.com/fonts/list/find_fonts/400?filter%5Blicense%5D%5B0%5D=web).
+* Audio's from [fontawesome](https://freesound.org/).
--- audio/app.js
+++ audio/app.js
@@ -1,5 +1,4 @@
 
-// define the needed variables
 let gameBoard = document.querySelectorAll(".cell");
 let playerTurn= true;
 let count=0;
@@ -7,7 +6,6 @@ let textArea = document.querySelector("#textArea");
 const modalBox = document.querySelector(".modal-box");
 const text = document.querySelector(".text");
 
-//for the score function
 const scoreX = document.querySelector(".playerX");
 const scoreO = document.querySelector(".playerO");
 const scoreTie = document.querySelector(".tie");
@@ -15,58 +13,41 @@ countScoreX=1;
 countScoreO=1;
 countScoreTie=1;
 
-//the audio's
 let audio = new Audio("audio/woohoo.mp3");
 let audio1 = new Audio("audio/lose.wav");
 
-
-//start functio n
 const startGame = function(){
-
-    // to check if the first turn true its X
-    if(playerTurn === true){
+if(playerTurn === true){
     this.innerText= "X";
     playerTurn=false;
     textArea.innerText="It's O turn";
 }
-// otherwise it's O
 else{
     this.innerText= "O";
     playerTurn= true;
     textArea.innerText="It's X turn";}
-
-// counter that will count each move to see if its a tie
 count++;
 console.log(count);
-//call the winner method
 winner();
 
-//to not repeat the click on the clicked cells
+//do not repeat the click on the clicked cells
 this.removeEventListener("click", startGame)
-} //end 1
-
-
-//to start the function after clicking
+}
 for(let i=0 ; i<gameBoard.length ; i++){
     gameBoard[i].addEventListener("click", startGame)
 }
 
-//the shown message of the winner
 const whoWon = function(){
     if(playerTurn == true){
         console.log("entered");
         audio.play();
-        // insert the text to the modal
         text.innerHTML+="<p>O is the winner</p>";
-        // to count the score of O
         scoreO.innerText=countScoreO++;
     }
     else if(playerTurn == false){
         console.log("entered");
         audio.play();
-        // insert the text to the modal
         text.innerHTML+="<p>X is the winner</p>";
-        // to count the score of X
         scoreX.innerHTML=countScoreX++;}
         
 
@@ -76,12 +57,10 @@ const whoWon = function(){
 for(let i=0 ; i<gameBoard.length ; i++){
     gameBoard[i].removeEventListener("click", startGame);}
 
-} // end 2
+}
 
-//the conditions of who won the game
 const winner = function(){
    
-    // define the variables
     let cell0 = document.getElementById("0"),
      cell1 = document.getElementById("1"),
      cell2 = document.getElementById("2"),
@@ -92,16 +71,13 @@ const winner = function(){
      cell7 = document.getElementById("7"),
      cell8 = document.getElementById("8");
 
-        //check if the cells has the same value 
+        
         if(cell0.innerHTML !== "" && cell0.innerHTML === cell1.innerHTML && cell0.innerHTML === cell2.innerHTML){
-            
             cell0.style.background="grey";
             cell1.style.background="grey";
             cell2.style.background="grey";
-            //call the message and audio function
             whoWon();
-            //display the modal
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
 
             }
 
@@ -109,9 +85,9 @@ const winner = function(){
         else if(cell3.innerHTML !== "" && cell3.innerHTML === cell4.innerHTML && cell3.innerHTML === cell5.innerHTML){
             cell3.style.background="grey";
             cell4.style.background="grey";
-            cell5.style.background="grey";   
+            cell5.style.background="grey";            
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
 
         else if(cell6.innerHTML !== "" && cell6.innerHTML === cell7.innerHTML && cell6.innerHTML === cell8.innerHTML){
@@ -119,7 +95,7 @@ const winner = function(){
             cell7.style.background="grey";
             cell8.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
 
         else if(cell0.innerHTML !== "" && cell0.innerHTML === cell3.innerHTML && cell0.innerHTML === cell6.innerHTML){
@@ -127,7 +103,7 @@ const winner = function(){
             cell3.style.background="grey";
             cell6.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
 
         else if(cell1.innerHTML !== "" && cell1.innerHTML === cell4.innerHTML && cell1.innerHTML === cell7.innerHTML){
@@ -135,14 +111,14 @@ const winner = function(){
             cell4.style.background="grey";
             cell7.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
         else if(cell2.innerHTML !== "" && cell2.innerHTML === cell5.innerHTML && cell2.innerHTML === cell8.innerHTML){
             cell2.style.background="grey";
             cell5.style.background="grey";
             cell8.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
 
         else if(cell0.innerHTML !== "" && cell0.innerHTML === cell3.innerHTML && cell0.innerHTML === cell6.innerHTML){
@@ -150,7 +126,7 @@ const winner = function(){
             cell3.style.background="grey";
             cell6.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
 
         else if(cell0.innerHTML !== "" && cell0.innerHTML === cell4.innerHTML && cell0.innerHTML === cell8.innerHTML){
@@ -158,7 +134,7 @@ const winner = function(){
             cell4.style.background="grey";
             cell8.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
 
         else if(cell2.innerHTML !== "" && cell2.innerHTML === cell4.innerHTML && cell2.innerHTML === cell6.innerHTML){
@@ -166,22 +142,20 @@ const winner = function(){
             cell4.style.background="grey";
             cell6.style.background="grey";
             whoWon();
-            modalBox.style.display = "block"; 
+            modalBox.style.display = 'block'; 
         }
-        
-        // to check if there's a tie and insert it in the tie box
         else if(count==9){
         textArea.innerText="It's a tie";
         scoreTie.innerHTML=countScoreTie++;
         audio1.play();
 
-    }} //end 3
+    }
+
+        }
 
 
-// new game function
 const newGame = function(){
-console.log("clicked");  
-//for loop to reset each cell to be empty, change the backgroun, the count ....     
+console.log("clicked");        
 for(let i=0 ; i<gameBoard.length ; i++){
     gameBoard[i].innerText="";
     gameBoard[i].style.background=" #ffffff url('image.png')";
@@ -191,11 +165,9 @@ for(let i=0 ; i<gameBoard.length ; i++){
     modalBox.style.display = "none"; 
     text.innerText="";
     gameBoard[i].addEventListener("click", startGame);
-}} // end 4
+}}
 
-// reset score function
 const ResetScore = function(){
-    // for loop to reset each cell to be empty, change background and reset the scores
     for(let i=0 ; i<gameBoard.length ; i++){
         gameBoard[i].innerText="";
         gameBoard[i].style.background=" #ffffff url('image.png')";
@@ -205,6 +177,8 @@ const ResetScore = function(){
         countScoreO= 1;
         scoreTie.innerText=0;
         countScoreTie= 1;
+
+
         gameBoard[i].addEventListener("click", startGame);
 }}
 
